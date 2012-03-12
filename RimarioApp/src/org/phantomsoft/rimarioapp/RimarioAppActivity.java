7674342fe5f69b956edc57a435b2c5e308b6cc7f
Sergio Bonfiglio <sergiobonfiglio@gmail.com>
2012-03-12 14:55:01 +0100

    prova

--- RimarioApp/src/org/phantomsoft/rimarioapp/RimarioAppActivity.java
+++ RimarioApp/src/org/phantomsoft/rimarioapp/RimarioAppActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class RimarioAppActivity extends Activity {
     /** Called when the activity is first created. */
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
