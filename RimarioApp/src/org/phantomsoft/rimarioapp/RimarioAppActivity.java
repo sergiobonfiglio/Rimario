0120fc199b4008519f1223f8b556a7935faefe32
Sergio Bonfiglio <sergiobonfiglio@gmail.com>
2012-03-12 14:42:14 +0100

    first commit

---
+++ RimarioApp/src/org/phantomsoft/rimarioapp/RimarioAppActivity.java
@@ -0,0 +1,13 @@
+package org.phantomsoft.rimarioapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RimarioAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
